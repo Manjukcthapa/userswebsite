f9b62a96384e0eaae321a7afe8df96f5d685cc2f
Manju KC <Manju KC>
2019-06-23 09:57:19 -0400

    added prod

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,18 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/ICN.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
